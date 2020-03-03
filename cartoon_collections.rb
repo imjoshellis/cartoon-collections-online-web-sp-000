903058f295354d181ca75d72fb18d534e0da7197
Josh Ellis <joshe523@gmail.com>
2020-03-03 03:51:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  arr.select do |item| 
+  arr.select do |item|
     cheese_types.each do |type|
       return item if item == type
     end
