436efb57de62bba69c23271ab8413b95182ac418
Josh Ellis <joshe523@gmail.com>
2020-03-03 03:48:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ def summon_captain_planet(arr)
   return arr.map { |item| "#{item.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.each { |item| item.size > 4 ? return true : nil }
+  return false
 end
 
 def find_the_cheese# code an argument here
